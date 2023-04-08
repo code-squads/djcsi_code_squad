429c58d8cde9ff6fcfcc1f801e3dff4cf7d0d3ce
Rupesh Raut <rupeshraut99396@gmail.com>
2023-04-08 18:48:26 +0530

    partial done

--- src/components/employeeVerificationForm.jsx
+++ src/components/employeeVerificationForm.jsx
@@ -24,6 +24,50 @@ const EmployeeVerificationForm = (
         setStage('verified')
     }
 
+    const ocr = async (options) => {
+        console.log("Inside ocr")
+        try {
+          const response = await axios.request(options);
+          console.log("Got response", response);
+          return response.data; 
+        } catch(err){
+          console.error(err);
+          return {"result":"1","subScans":[],"value":"HRE FRE\nGovernment of India\nAmitabh Rajendra Shah\nDOB: 03/02/1990\nMale\n1234 5678 9012\n\u00c5RT TEIR, \u00c0"};
+        }
+        // return {"result":"1","subScans":[],"value":"HRE FRE\nGovernment of India\nAmitabh Rajendra Shah\nDOB: 03/02/1990\nMale\n1234 5678 9012\n\u00c5RT TEIR, \u00c0"};
+    }
+
+    const handleFileInputChange = (event) => {
+        // setPreviewImage(URL.createObjectURL(event.target.files[0]));
+        console.log("inside handle file", event)
+        const form = new FormData();
+        form.append("Session", "string");
+        form.append("srcImg", event.target.files[0]);
+        const options = {
+          method: "POST",
+          url: "https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/",
+          headers: {
+            "content-type": "multipart/form-data",
+            "x-rapidapi-host": "pen-to-print-handwriting-ocr.p.rapidapi.com",
+            "x-rapidapi-key": process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
+          },
+          data: form,
+        };
+        // return
+        const dataText = ocr(options)
+          .then(res => {
+            // setText(res.value);
+            console.log(res.value)
+            const splitMe = res.value.split('\n')
+            const userName = splitMe[2];
+            const DOB = splitMe[3].split(':')[1].trim();
+            const aadhar = splitMe[5];
+            console.log(userName);
+            console.log(DOB);
+            console.log(aadhar);
+          });
+      }
+
     return (
         <form className="mt-[40px]" action="">
             <div className="flex flex-col gap-y-[25px]">
@@ -69,7 +113,7 @@ const EmployeeVerificationForm = (
                     </div>
                 </div>
 
-                <div className="flex flex-row gap-x-[30px]">
+                <div className="flex flex-row gap-x-[30px] justify-between">
                     <div className="w-[45%] relative">
                         <label htmlFor="cardNumber" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Aadhaar/Pan card number</label>
                         <input type="text" id="cardNumber" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 uppercase" maxLength={15} 
@@ -78,6 +122,19 @@ const EmployeeVerificationForm = (
                         {stage == 'verified' && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="green" className="absolute top-[39px] right-[20px] w-5 h-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>}
+                    </div>
+
+                    <div className="flex flex-col w-[45%]">
+                        <div className="flex items-center justify-center w-full">
+                            <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-[70px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div className="flex flex-col items-center justify-center">
+                                    <svg aria-hidden="true" className="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                                    <p className="mb-2 text-[10px] text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p className="text-[8px] text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input id="dropzone-file" type="file" className="hidden" onChange={handleFileInputChange}/>
+                            </label>
+                        </div> 
                     </div>
                 </div>
 
@@ -182,12 +239,13 @@ const EmployeeVerificationForm = (
                     </div>
                 </div> */}
 
-                {stage == "verified" && <div className="flex flex-row gap-x-[30px]">
+                {/* {stage == "verified" && <div className="flex flex-row gap-x-[30px]">
                     <div className="flex flex-col w-[45%]">
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                         <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required/>
-                    </div> 
-                </div>}
+                    </div>
+                    
+                </div>} */}
             </div>
             
             <div className="flex flex-row gap-x-[20px] mt-[45px] items-center justify-center">
@@ -204,9 +262,9 @@ const EmployeeVerificationForm = (
                 </div> */}
             </div>
 
-            <div className="text-[12px] text-[#6A7280] mt-[15px]">
+            {/* <div className="text-[12px] text-[#6A7280] mt-[15px]">
                 By creating account you agree to the terms and conditions
-            </div>
+            </div> */}
         </form>
     )
 }
--- src/pages/singleEmployee.jsx
+++ src/pages/singleEmployee.jsx
@@ -1,9 +1,15 @@
+import EmployeeVerificationForm from '@/components/employeeVerificationForm'
 import React from 'react'
 
 const SingleEmployee = () => {
   return (
-    <div>
-        Single Employee
+    <div className='w-[70%] dark:text-white dark:bg-dark2 text-dark3'>
+        <div className='flex flex-row items-center text-[26px] font-medium h-[70px] pl-[40px] border-b-[1px] dark:border-[#232830] border-[#DCE3EE] bg-'>
+            Verify Employee
+        </div>
+        <div className='w-[60%] mx-auto'>
+            <EmployeeVerificationForm/>
+        </div>
     </div>
   )
 }
