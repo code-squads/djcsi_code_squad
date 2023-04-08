3d72c8697d0deceb8040079770e61dfdc104329b
vansh <vanshbaghel912@gmail.com>
2023-04-08 20:16:07 +0530

    Safe and danger button text changed

--- src/components/AppGrid.js
+++ src/components/AppGrid.js
@@ -21,14 +21,14 @@ function RenderDate(props) {
 
   return (
     <>
-      {flagColor === "red" && (
+      {flagColor === "green" && (
         <button
           className="focus:outline-none text-white w-[8rem] bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
           Safe User
         </button>
       )}
-      {flagColor === "green" && (
+      {flagColor === "red" && (
         <button 
           className="focus:outline-none text-white w-[8rem] bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
         >
